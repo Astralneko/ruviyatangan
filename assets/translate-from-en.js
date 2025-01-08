c56fe5f1a274e24e960d18835ffae97a9df85851
Astralneko <90540803+Astralneko@users.noreply.github.com>
2025-01-08 14:55:56 -0600

    Fix sidebar + add first article

--- assets/translate-from-en.js
+++ assets/translate-from-en.js
@@ -1,5 +1,5 @@
 		const spanish_link = "/spa" + window.location.pathname + window.location.search;
 		const rokadong_link = "/rkd" + window.location.pathname + window.location.search;
 		
-		document.getElementById("spanish-page").href=spanish_link;
-		document.getElementById("rokadong-page").href=rokadong_link;
+		document.getElementsByClassName("spanish-page")[0].href=spanish_link;
+		document.getElementsByClassName("rokadong-page")[0].href=rokadong_link;

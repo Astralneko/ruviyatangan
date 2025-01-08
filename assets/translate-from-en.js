48b23fd6d3f08556bfe2670a4d97646692263708
Astralneko <90540803+Astralneko@users.noreply.github.com>
2025-01-08 04:27:38 -0600

    Fix en translate

---
+++ assets/translate-from-en.js
@@ -0,0 +1,5 @@
+		const spanish_link = "/spa" + window.location.pathname + window.location.search;
+		const rokadong_link = "/rkd" + window.location.pathname + window.location.search;
+		
+		document.getElementById("spanish-page").href=spanish_link;
+		document.getElementById("rokadong-page").href=rokadong_link;
